bc00f5130f1240385f2cf976a35b21a78ab77d48
Aalysher <aalysher@gmail.com>
2023-07-26 00:57:10 +0600

    draft fix router

--- src/back/interfaces/router.py
+++ src/back/interfaces/router.py
@@ -1,10 +1,13 @@
-from typing import Self, Protocol
-from tornado.iostream import IOStream
+"""Module containing which serves as an interface for handling messages in the router."""
+from typing import Protocol, Self
+
+from src.back.interfaces.io import WriteStreamInterface
+from src.back.interfaces.message import MessageInterface
 
 
 class MessageHandlerInterface(Protocol):
     """Interface for handling messages in the router."""
 
-    def handle(self: Self, message: bytes, stream: IOStream) -> None:
+    async def handle( self: Self, message: MessageInterface, stream: WriteStreamInterface) -> None:
         """Process the incoming message."""
         raise NotImplementedError
--- src/back/router.py
+++ src/back/router.py
@@ -1,9 +1,28 @@
-from typing import Self
-from tornado.iostream import IOStream
+from typing import Any, Self
 
 
-class Router(MessageHandlerInterface):
-    """"""
+from src.back.interfaces.message import MessageInterface
+from src.back.interfaces.router import MessageHandlerInterface
+from src.back.interfaces.io import WriteStreamInterface
 
-    def handle(self: Self, message: bytes, stream: IOStream) -> None:
-        """"""
+
+class RoutingHandler(MessageHandlerInterface[MessageInterface]):
+    """A message handler that routes incoming messages to appropriate handlers based on their type."""
+
+    message_type_to_handlers: dict[ type[MessageInterface], tuple[MessageHandlerInterface[Any], ...]]
+
+    def __init__(
+        self: Self,
+        message_type_to_handlers: dict[
+            type[MessageInterface],
+            tuple[MessageHandlerInterface[Any], ...],
+        ],
+    ) -> None:
+        self.message_type_to_handlers = message_type_to_handlers
+
+    async def handle( self: Self, message: MessageInterface, stream: WriteStreamInterface) -> None:
+        """Process the incoming message."""
+        handlers = self.message_type_to_handlers.get(type(message), ())
+
+        for handler in handlers:
+            await handler.handle(message, stream)
